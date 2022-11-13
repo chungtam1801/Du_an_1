5e2c44eac2c56659a5c430bc9971c98fe3c1a6fa
IdolTohancauu <quynbph20039@fpt.edu.vn>
2022-11-13 23:16:52 +0700

    kichthuoc

---
+++ 3.PL/Views/Frm_KichThuoc.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace _3.PL.Views
+{
+    partial class Frm_KichThuoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_ma = new System.Windows.Forms.TextBox();
+            this.tb_size = new System.Windows.Forms.TextBox();
+            this.rbtn_conhang = new System.Windows.Forms.RadioButton();
+            this.rbtn_hethang = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtg_KichThuoc = new System.Windows.Forms.DataGridView();
+            this.btn_them = new System.Windows.Forms.Button();
+            this.btn_sua = new System.Windows.Forms.Button();
+            this.btn_xoa = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_KichThuoc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_ma
+            // 
+            this.tb_ma.Location = new System.Drawing.Point(120, 30);
+            this.tb_ma.Name = "tb_ma";
+            this.tb_ma.Size = new System.Drawing.Size(187, 23);
+            this.tb_ma.TabIndex = 0;
+            // 
+            // tb_size
+            // 
+            this.tb_size.Location = new System.Drawing.Point(120, 80);
+            this.tb_size.Name = "tb_size";
+            this.tb_size.Size = new System.Drawing.Size(187, 23);
+            this.tb_size.TabIndex = 1;
+            // 
+            // rbtn_conhang
+            // 
+            this.rbtn_conhang.AutoSize = true;
+            this.rbtn_conhang.Location = new System.Drawing.Point(437, 31);
+            this.rbtn_conhang.Name = "rbtn_conhang";
+            this.rbtn_conhang.Size = new System.Drawing.Size(77, 19);
+            this.rbtn_conhang.TabIndex = 2;
+            this.rbtn_conhang.TabStop = true;
+            this.rbtn_conhang.Text = "Còn hàng";
+            this.rbtn_conhang.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_hethang
+            // 
+            this.rbtn_hethang.AutoSize = true;
+            this.rbtn_hethang.Location = new System.Drawing.Point(437, 65);
+            this.rbtn_hethang.Name = "rbtn_hethang";
+            this.rbtn_hethang.Size = new System.Drawing.Size(74, 19);
+            this.rbtn_hethang.TabIndex = 3;
+            this.rbtn_hethang.TabStop = true;
+            this.rbtn_hethang.Text = "Hết hàng";
+            this.rbtn_hethang.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mã";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Size";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(372, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Trạng thái";
+            // 
+            // dtg_KichThuoc
+            // 
+            this.dtg_KichThuoc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_KichThuoc.Location = new System.Drawing.Point(12, 169);
+            this.dtg_KichThuoc.Name = "dtg_KichThuoc";
+            this.dtg_KichThuoc.RowTemplate.Height = 25;
+            this.dtg_KichThuoc.Size = new System.Drawing.Size(540, 150);
+            this.dtg_KichThuoc.TabIndex = 7;
+            this.dtg_KichThuoc.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtg_KichThuoc_CellClick);
+            // 
+            // btn_them
+            // 
+            this.btn_them.Location = new System.Drawing.Point(24, 124);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(109, 29);
+            this.btn_them.TabIndex = 11;
+            this.btn_them.Text = "Thêm";
+            this.btn_them.UseVisualStyleBackColor = true;
+            this.btn_them.Click += new System.EventHandler(this.btn_them_Click);
+            // 
+            // btn_sua
+            // 
+            this.btn_sua.Location = new System.Drawing.Point(160, 124);
+            this.btn_sua.Name = "btn_sua";
+            this.btn_sua.Size = new System.Drawing.Size(109, 29);
+            this.btn_sua.TabIndex = 12;
+            this.btn_sua.Text = "Sửa";
+            this.btn_sua.UseVisualStyleBackColor = true;
+            this.btn_sua.Click += new System.EventHandler(this.btn_sua_Click);
+            // 
+            // btn_xoa
+            // 
+            this.btn_xoa.Location = new System.Drawing.Point(300, 124);
+            this.btn_xoa.Name = "btn_xoa";
+            this.btn_xoa.Size = new System.Drawing.Size(109, 29);
+            this.btn_xoa.TabIndex = 13;
+            this.btn_xoa.Text = "Xóa";
+            this.btn_xoa.UseVisualStyleBackColor = true;
+            this.btn_xoa.Click += new System.EventHandler(this.btn_xoa_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(437, 124);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(109, 29);
+            this.btn_clear.TabIndex = 14;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Frm_KichThuoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 336);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_xoa);
+            this.Controls.Add(this.btn_sua);
+            this.Controls.Add(this.btn_them);
+            this.Controls.Add(this.dtg_KichThuoc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbtn_hethang);
+            this.Controls.Add(this.rbtn_conhang);
+            this.Controls.Add(this.tb_size);
+            this.Controls.Add(this.tb_ma);
+            this.Name = "Frm_KichThuoc";
+            this.Text = "Frm_KichThuoc";
+            this.Load += new System.EventHandler(this.Frm_KichThuoc_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_KichThuoc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_ma;
+        private System.Windows.Forms.TextBox tb_size;
+        private System.Windows.Forms.RadioButton rbtn_conhang;
+        private System.Windows.Forms.RadioButton rbtn_hethang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dtg_KichThuoc;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.Button btn_sua;
+        private System.Windows.Forms.Button btn_xoa;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
---
+++ 3.PL/Views/Frm_KichThuoc.cs
@@ -0,0 +1,116 @@
+﻿using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+using _1.DAL.Repositories;
+using _2.BUS.IServices;
+using _2.BUS.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.PL.Views
+{
+    public partial class Frm_KichThuoc : Form
+    {
+        private IKichThuocRepository _ikichThuocRepository;
+        private IQLKichThuocServices _iqlkichThuocServices;
+        private Guid _id;
+        public Frm_KichThuoc()
+        {
+            _ikichThuocRepository = new KichThuocRepository();
+            _iqlkichThuocServices = new QLKichThuocServices();
+            InitializeComponent();
+        }
+        private void LoadData()
+        {
+            dtg_KichThuoc.Rows.Clear();
+            dtg_KichThuoc.ColumnCount = 4;
+            dtg_KichThuoc.Columns[0].Name = "ID";
+            dtg_KichThuoc.Columns[1].Name = "Mã";
+            dtg_KichThuoc.Columns[2].Name = "Size";
+            dtg_KichThuoc.Columns[3].Name = "Trạng thái";
+            foreach (var x in _iqlkichThuocServices.GetAll())
+            {
+                dtg_KichThuoc.Rows.Add(x.Id, x.Ma, x.Size, x.TrangThai == 1? "Còn hàng" : "Hết hàng");
+            }
+        }
+        private KichThuoc GetDataFromGUI()
+        {
+            KichThuoc kt = new KichThuoc();
+            kt.Id = _id;
+            kt.Ma = tb_ma.Text;
+            kt.Size = tb_size.Text;
+            if (rbtn_conhang.Checked == true)
+            {
+                kt.TrangThai = 1;
+            }
+            else if (rbtn_hethang.Checked == true)
+            {
+                kt.TrangThai = 0;
+            }
+            return kt;
+        }
+
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Bạn có muốn thêm không?", "", MessageBoxButtons.YesNo))
+            {
+                _iqlkichThuocServices.Add(GetDataFromGUI());
+                LoadData();
+            }
+        }
+
+        private void btn_sua_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Bạn có muốn sửa không?", "", MessageBoxButtons.YesNo))
+            {
+                _iqlkichThuocServices.Update(GetDataFromGUI());
+                LoadData();
+            }
+        }
+
+        private void btn_xoa_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Bạn có muốn xóa không?", "", MessageBoxButtons.YesNo))
+            {
+                _iqlkichThuocServices.Delete(GetDataFromGUI());
+                LoadData();
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            tb_ma.Text = "";
+            tb_size.Text = "";
+            rbtn_conhang.Checked = true;
+            LoadData();
+        }
+
+        private void dtg_KichThuoc_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            var kt = _iqlkichThuocServices.GetAll().FirstOrDefault(c => c.Id == Guid.Parse(dtg_KichThuoc.Rows[rowIndex].Cells[0].Value.ToString()));
+            _id = kt.Id;
+            tb_ma.Text = kt.Ma;
+            tb_size.Text = kt.Size;
+            if (kt.TrangThai == 1)
+            {
+                rbtn_conhang.Checked = true;
+            }
+            else if (kt.TrangThai == 0)
+            {
+                rbtn_hethang.Checked = true;
+            }
+        }
+
+        private void Frm_KichThuoc_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
