08f3b1606bd5c99ff81898fa17797265f8bf385b
huongdtlph25484 <huongdtlph25484@fpt.edu.vn>
2022-11-15 17:39:46 +0700

    bo sung chuc vu

---
+++ 3.PL/Views/Frm_ChucVu.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace _3.PL.Views
+{
+    partial class Frm_ChucVu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.rbtn_kohd = new System.Windows.Forms.RadioButton();
+            this.rbtn_hd = new System.Windows.Forms.RadioButton();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.dgvChucVu = new System.Windows.Forms.DataGridView();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChucVu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_clear.Location = new System.Drawing.Point(611, 160);
+            this.btn_clear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(82, 22);
+            this.btn_clear.TabIndex = 40;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // rbtn_kohd
+            // 
+            this.rbtn_kohd.AutoSize = true;
+            this.rbtn_kohd.Location = new System.Drawing.Point(513, 85);
+            this.rbtn_kohd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rbtn_kohd.Name = "rbtn_kohd";
+            this.rbtn_kohd.Size = new System.Drawing.Size(118, 19);
+            this.rbtn_kohd.TabIndex = 39;
+            this.rbtn_kohd.TabStop = true;
+            this.rbtn_kohd.Text = "Không hoạt động";
+            this.rbtn_kohd.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_hd
+            // 
+            this.rbtn_hd.AutoSize = true;
+            this.rbtn_hd.Location = new System.Drawing.Point(513, 45);
+            this.rbtn_hd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rbtn_hd.Name = "rbtn_hd";
+            this.rbtn_hd.Size = new System.Drawing.Size(82, 19);
+            this.rbtn_hd.TabIndex = 38;
+            this.rbtn_hd.TabStop = true;
+            this.rbtn_hd.Text = "Hoạt động";
+            this.rbtn_hd.UseVisualStyleBackColor = true;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Label4.Location = new System.Drawing.Point(399, 69);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(63, 15);
+            this.Label4.TabIndex = 37;
+            this.Label4.Text = "Trạng thái:";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Label3.Location = new System.Drawing.Point(63, 126);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(74, 15);
+            this.Label3.TabIndex = 36;
+            this.Label3.Text = "Tên chức vụ:";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Label2.Location = new System.Drawing.Point(63, 66);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(72, 15);
+            this.Label2.TabIndex = 35;
+            this.Label2.Text = "Mã chức vụ:\r\n";
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSua.Location = new System.Drawing.Point(275, 160);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(82, 22);
+            this.btnSua.TabIndex = 34;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnXoa.Location = new System.Drawing.Point(436, 160);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(82, 22);
+            this.btnXoa.TabIndex = 33;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnThem.Location = new System.Drawing.Point(106, 160);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(82, 22);
+            this.btnThem.TabIndex = 32;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // dgvChucVu
+            // 
+            this.dgvChucVu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChucVu.Location = new System.Drawing.Point(85, 195);
+            this.dgvChucVu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvChucVu.Name = "dgvChucVu";
+            this.dgvChucVu.RowHeadersWidth = 51;
+            this.dgvChucVu.RowTemplate.Height = 29;
+            this.dgvChucVu.Size = new System.Drawing.Size(653, 211);
+            this.dgvChucVu.TabIndex = 31;
+            this.dgvChucVu.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvChucVu_CellClick);
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(185, 116);
+            this.txtTen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(110, 23);
+            this.txtTen.TabIndex = 30;
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(185, 64);
+            this.txtMa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(110, 23);
+            this.txtMa.TabIndex = 29;
+            // 
+            // Frm_ChucVu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.rbtn_kohd);
+            this.Controls.Add(this.rbtn_hd);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgvChucVu);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtMa);
+            this.Name = "Frm_ChucVu";
+            this.Text = "Frm_ChucVu";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChucVu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.RadioButton rbtn_kohd;
+        private System.Windows.Forms.RadioButton rbtn_hd;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.DataGridView dgvChucVu;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtMa;
+    }
+}
