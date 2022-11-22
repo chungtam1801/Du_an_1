4d97fb60049f575ce46abb68dffc9b20efb4e211
Nhinhph25737 <nhinhphph25737@fpt.edu.vn>
2022-11-23 00:25:56 +0700

    Các Form Thêm Nhanh

---
+++ 3.PL/Views/Frm_ThemNhanhChatLieu.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace _3.PL.Views
+{
+    partial class Frm_ThemNhanhChatLieu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_boqua = new System.Windows.Forms.Button();
+            this.btn_luu = new System.Windows.Forms.Button();
+            this.tbx_ten = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(169, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Thêm Chất liệu";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn_boqua
+            // 
+            this.btn_boqua.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btn_boqua.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_boqua.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_boqua.Image = global::_3.PL.Properties.Resources.boqua;
+            this.btn_boqua.Location = new System.Drawing.Point(276, 138);
+            this.btn_boqua.Name = "btn_boqua";
+            this.btn_boqua.Size = new System.Drawing.Size(98, 44);
+            this.btn_boqua.TabIndex = 16;
+            this.btn_boqua.Text = "Bỏ qua";
+            this.btn_boqua.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_boqua.UseVisualStyleBackColor = false;
+            // 
+            // btn_luu
+            // 
+            this.btn_luu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(114)))), ((int)(((byte)(142)))));
+            this.btn_luu.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_luu.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_luu.Image = global::_3.PL.Properties.Resources.save;
+            this.btn_luu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_luu.Location = new System.Drawing.Point(144, 138);
+            this.btn_luu.Name = "btn_luu";
+            this.btn_luu.Size = new System.Drawing.Size(98, 44);
+            this.btn_luu.TabIndex = 15;
+            this.btn_luu.Text = "     Lưu";
+            this.btn_luu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_luu.UseVisualStyleBackColor = false;
+            // 
+            // tbx_ten
+            // 
+            this.tbx_ten.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbx_ten.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbx_ten.Location = new System.Drawing.Point(144, 59);
+            this.tbx_ten.Multiline = true;
+            this.tbx_ten.Name = "tbx_ten";
+            this.tbx_ten.Size = new System.Drawing.Size(230, 23);
+            this.tbx_ten.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(29)))), ((int)(((byte)(54)))));
+            this.panel1.Location = new System.Drawing.Point(144, 86);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 2);
+            this.panel1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(29, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Tên Chất liệu:";
+            // 
+            // Frm_ThemNhanhChatLieu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 198);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_boqua);
+            this.Controls.Add(this.btn_luu);
+            this.Controls.Add(this.tbx_ten);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Location = new System.Drawing.Point(500, 100);
+            this.Name = "Frm_ThemNhanhChatLieu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Frm_ThemNhanhChatLieu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_boqua;
+        private System.Windows.Forms.Button btn_luu;
+        private System.Windows.Forms.TextBox tbx_ten;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
