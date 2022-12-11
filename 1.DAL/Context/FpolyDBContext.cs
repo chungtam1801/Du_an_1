03d680de3a9a9b50c2be7a25238cb87d9ed93e2c
lamtran <lamtran6131@gmail.com>
2022-12-11 13:29:17 +0700

    suakh

--- 1.DAL/Context/FpolyDBContext.cs
+++ 1.DAL/Context/FpolyDBContext.cs
@@ -42,7 +42,7 @@ namespace _1.DAL.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source=oh\\sqlexpress;Initial Catalog=FINALASS_BanQuanAo_Nhom666_FA22_PRO131;Persist Security Info=True;User ID=sa;Password=111111");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-S6G7NFV\\SQLEXPRESS;Initial Catalog=FINALASS_BanQuanAo_Nhom666_FA22_PRO131;Persist Security Info=True;User ID=sa;Password=123456");
             }
         }
 
